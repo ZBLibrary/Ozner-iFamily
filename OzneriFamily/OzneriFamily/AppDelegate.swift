eec817b1f8dcb997659ccb900358922f8570d497
airfight <745756619@qq.com>
2017-04-24 15:23:21 +0800

    //测试

--- OzneriFamily/OzneriFamily/AppDelegate.swift
+++ OzneriFamily/OzneriFamily/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,WXApiDelegate,UNUserNotifi
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        
+        //
         
         window?.backgroundColor = UIColor.white
         
